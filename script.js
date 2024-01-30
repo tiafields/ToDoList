3dcc98053f25face904a502929f5d2e1176de11c
Tia Fields <tia.fields226@gmail.comy>
2024-01-30 10:24:31 -0500

    adding functions to save data and display it in the browser

--- script.js
+++ script.js
@@ -15,19 +15,32 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value ="";
+    saveData();
 }
 /* Processing task by hitting the Enter Button on the Keyboard */
 inputBox.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         addTask();
+        savaData();
     }
 });
 /*Function for marking task on or off the list */
 listContainter.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 },false);
+/* Saving Data  */
+function saveData(){
+    localStorage.setItem("data", listContainter.innerHTML);
+}
+/* Displaying data when opening broswer*/
+function showTask(){
+    listContainter.innerHTML = localStorage.getItem("data");
+}
+showTask();
