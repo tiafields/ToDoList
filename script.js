dafccce80e5a5a63f4f3d306e7d7e6d3c4c7f625
Tia Fields <tia.fields226@gmail.comy>
2024-01-29 23:04:36 -0500

    chanign styling to text field, preparing the UI of the delete task button, adding an EventListener to add a task with by hitting the enter key

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainter = document.getElementById("list-container");
 
+/* Function for Adding Task */
 function addTask(){
     if(inputBox.value === '') {
         alert("Please enter a task.");
@@ -9,7 +10,15 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainter.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    
-
+    inputBox.value ="";
 }
+/* Processing task by hitting the Enter Button on the Keyboard */
+inputBox.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
